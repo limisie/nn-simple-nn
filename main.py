3ce1f8a288f77d53368cc7cc29dccdec17a7e1ec
limisie <k.limisiewicz@gmail.com>
2021-10-08 18:41:46 +0200

    feat: add Neuron abstract class

--- main.py
+++ main.py
@@ -1,57 +1,144 @@
+import sys
+import time
+from abc import ABC, abstractmethod
 import numpy as np
 
-input_data = np.array([
-    [0, 0],
-    [0, 1],
-    [1, 0],
-    [1, 1]
-])
-classes = [0, 1, 1, 1]
-weights = np.random.rand(len(input_data[0]) + 1)
-learning_rate = 0.01
+from data import x_train, y_train, x_test, y_test
 
 
-def weighted_sum(x, w):
-    sum = w[0]
-    for i in range(len(x)):
-        sum += w[i + 1] * x[i]
-    return sum
+class Neuron(ABC):
+    def __init__(self, w_range, alpha):
+        self.w_range = w_range
+        self.alpha = alpha
+        self.w = []
+        self.y = []
+        self.epochs = 0
+        self.time = 0
+
+        self.net_error = sys.maxsize
+
+    @abstractmethod
+    def activation_function(self, weighted_sum):
+        pass
+
+    def weighted_sums(self, x):
+        return self.w[0] + np.dot(x, self.w[1:])
+
+    def predictions(self, x):
+        return np.array([self.activation_function(z) for z in self.weighted_sums(x)])
+
+    def update_weights(self, x, error):
+        self.w[0] = self.w[0] + self.alpha * np.sum(error)
+        self.w[1:] += np.dot(error, x) * self.alpha
+
+    def error(self, d, y):
+        return d - y
+
+    def calculate_net_error(self, errors):
+        self.net_error = np.sum(abs(errors))
+
+    def stop_condition(self):
+        return self.net_error == 0
+
+    def train(self, x, d):
+        self.w = np.random.uniform(low=-self.w_range, high=self.w_range, size=(len(x[0]) + 1,))
+        self.epochs = 0
+        self.time = time.time()
+
+        while not self.stop_condition():
+            self.y = self.predictions(x)
+            errors = self.error(d, self.y)
+            self.calculate_net_error(errors)
+            if self.net_error != 0:
+                self.update_weights(x, errors)
+
+            self.epochs += 1
+
+        self.time = time.time() - self.time
+        self.net_error = sys.maxsize
+
+    def evaluate(self, x):
+        self.y = self.predictions(x)
 
 
-def binary_activation_function(weighted_sum):
-    return int(weighted_sum > 0)
+class PerceptronUnipolar(Neuron):
+
+    def activation_function(self, weighted_sum):
+        return int(weighted_sum > 0)
 
 
-def update_weights(x, w, error, alpha):
-    w[0] = w[0] + alpha * error
+class PerceptronBipolar(Neuron):
 
-    for i in range(len(w) - 1):
-        w[i + 1] = w[i + 1] + error * alpha * x[i]
+    def activation_function(self, weighted_sum):
+        if weighted_sum > 0:
+            return 1
+        else:
+            return -1
 
-    return w
+    def train(self, x, d):
+        x[x < 0.5] -= 1
+        d[d == 0] = -1
+
+        super().train(x, d)
+
+        d[d == -1] = -0
+        self.y[self.y == -1] = 0
+
+    def evaluate(self, x):
+        x[x < 0.5] -= 1
+
+        super().evaluate(x)
+
+        self.y[self.y == -1] = 0
 
 
-def stop_condition(delta_sum):
-    return delta_sum == 0
+class Adaline(PerceptronBipolar):
+    def __init__(self, w_range, alpha, allowed_error):
+        super().__init__(w_range, alpha)
+        self.allowed_error = allowed_error
 
+    def predictions(self, x):
+        return self.weighted_sums(x)
 
-def perceptron(x, w, alpha, d):
-    delta_sum = -1
-    predictions = np.empty(len(x))
+    def stop_condition(self):
+        return self.net_error < self.allowed_error
 
-    while not stop_condition(delta_sum):
-        delta_sum = 0
-        predictions = np.empty(len(x))
+    def calculate_net_error(self, errors):
+        self.net_error = np.sum(errors ** 2) / (len(self.w) - 1)
 
-        for i, xi in enumerate(x):
-            predictions[i] = binary_activation_function(weighted_sum(xi, w))
-            error = d[i] - predictions[i]
-            delta_sum += abs(error)
-            update_weights(xi, w, error, alpha)
+    def train(self, x, d):
+        x[x < 0.5] -= 1
+        d[d == 0] = -1
 
-    print(w)
-    return predictions
+        Neuron.train(self, x, d)
+
+        self.y = np.array([self.activation_function(y) for y in self.y])
+        d[d == -1] = -0
+        self.y[self.y == -1] = 0
+
+    def evaluate(self, x):
+        x[x < 0.5] -= 1
+
+        Neuron.evaluate(self, x)
+
+        self.y = np.array([self.activation_function(y) for y in self.y])
+        self.y[self.y == -1] = 0
 
 
 if __name__ == '__main__':
-    print(perceptron(input_data, weights, learning_rate, classes))
+    learning_rate = 0.01
+    weight_range = 0.01
+    iterations = 100
+    epochs = 0
+    times = 0
+    allowed_error = 0.1
+
+    perceptron = PerceptronUnipolar(weight_range, learning_rate)
+
+    for i in range(iterations):
+        perceptron.train(x_train, y_train)
+        epochs += perceptron.epochs
+        times += perceptron.time
+
+    print(epochs / iterations)
+    print(times / iterations)
